049ccb1b0d4db0f753c120dabb7b46cb3392fefc
roukee <thimomerke@gmail.com>
2015-10-20 21:46:45 +0200

    removed old files

--- STTTest/src/de/roukee/test/Ente.java
+++
@@ -1,7 +0,0 @@
-package de.roukee.test;
-
-public class Ente {
- String meinName(){
-	 return "Mein Name ist Dagobert!";
- }
-}
--- STTTest/src/de/roukee/test/main.java
+++
@@ -1,8 +0,0 @@
-package de.roukee.test;
-
-public class main {
-	public static void main(String[] args){
-		 Ente dagobert = new Ente();
-		 System.out.println(dagobert.meinName());
-	 }
-}
